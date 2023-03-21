307034f4376dfe6977ea1970e8e4484fbe683f6e
hyuntae <believe4279@gmail.com>
2023-03-21 15:47:49 +0900

    매물리스트 사진추가

--- src/components/kaKaoMap/showList/show.tsx
+++ src/components/kaKaoMap/showList/show.tsx
@@ -14,29 +14,31 @@ const Show = ({ data }: any) => {
     return <div className={cx("noItem")}>매물이 없습니다!</div>;
   return (
     <div className={cx("list_wrapper")}>
-      {data.map((item: any) => {
-        return (
-          <div
-            className={cx("house")}
-            key={item.id}
-            onClick={() => router.push(`/detail/${item.id}`)}
-          >
-            <img className={cx("houseImg")}></img>
-            <div className={cx("content_wrapper")}>
-              <div className={cx("houseType")}>{item.houseType}</div>
-              <div className={cx("houseName")}>{item.houseName}</div>
-              <div className={cx("price")}>
-                <span className={cx("selected")}>{item.price}</span>
-              </div>
-              <div className={cx("netLeasableArea")}>
-                {item.netLeasableArea + " / 관리비 10만"}
-              </div>
-              <p>★역세권/3분거리/신축건물/풀옵션★</p>
-              <p>☞☞☞전화,문자 문의 가능</p>
+      {data.map((item: any, idx: number) => (
+        <div
+          className={cx("house")}
+          key={item.id}
+          onClick={() => router.push(`/detail/${item.id}`)}
+        >
+          <img
+            className={cx("houseImg")}
+            src={`/img/img${idx % 9}.jpg`}
+            alt="매물사진"
+          ></img>
+          <div className={cx("content_wrapper")}>
+            <div className={cx("houseType")}>{item.houseType}</div>
+            <div className={cx("houseName")}>{item.houseName}</div>
+            <div className={cx("price")}>
+              <span className={cx("selected")}>{item.price}</span>
             </div>
+            <div className={cx("netLeasableArea")}>
+              {item.netLeasableArea + ` / 관리비 ${item.maintenanceCost}만`}
+            </div>
+            <p>★역세권/3분거리/신축건물/풀옵션★</p>
+            <p>☞☞☞전화,문자 문의 가능</p>
           </div>
-        );
-      })}
+        </div>
+      ))}
     </div>
   );
 };
